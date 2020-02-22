0e07e3b3b7be99553c18eef04a1d3065eb82fc63
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2020-02-22 01:23:46 -0500

    Create the first part of the Scripture Journal App, until now  it's working perfectly, I should add search option, and give the design

---
+++ My Scripture Journal/Models/Scripture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace My_Scripture_Journal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Book { get; set; }
+
+        [Display(Name = "Added Date"), DataType(DataType.Date)]
+        [Required]
+        public DateTime AddedDate { get; set; }
+
+        //[Column(TypeName = "integer(18)")]
+        [Required]
+        public int Chapter { get; set; }
+
+        //[Column(TypeName = "integer(18)")]
+        [Required]
+        public int Verse { get; set; }
+
+        [StringLength(250, MinimumLength = 10)]
+        [Required]
+        public string Note { get; set; }
+    }
+}
