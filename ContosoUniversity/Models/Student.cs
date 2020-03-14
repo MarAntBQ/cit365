aaf22468c3e056f587a62f0e4ae5e94e0f361284
Marbust CEO <Marbust CEO@MARBUSTCEO>
2020-03-14 09:36:22 -0500

    Make the corrections to the flags in the model done de 1/10

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        
+        [Display(Name = "Enrollment Date"), DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
