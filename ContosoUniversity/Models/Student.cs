7e36209ab693522954f035bec2bb4fcd45f81140
Marbust CEO <Marbust CEO@MARBUSTCEO>
2020-03-14 16:30:00 -0500

    dotnet ef migrations add ColumnFirstName

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -2,27 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
-        [Required]
         [StringLength(50)]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required]
         [StringLength(50)]
-        [Display(Name = "First Name")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
-
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
