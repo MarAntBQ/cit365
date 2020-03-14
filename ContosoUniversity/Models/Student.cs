62550845ba590718caa9a47d86d5fcc363a6aad6
Marbust CEO <Marbust CEO@MARBUSTCEO>
2020-03-14 09:55:14 -0500

    Before "Actualizar la página Create" https://docs.microsoft.com/es-es/aspnet/core/data/ef-mvc/crud?view=aspnetcore-2.2#update-the-create-page

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,19 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
-        
+        [Required]
+        [StringLength(50)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
+        [Required]
+        [StringLength(50)]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
-        
-        [Display(Name = "Enrollment Date"), DataType(DataType.Date)]
-        public DateTime EnrollmentDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
